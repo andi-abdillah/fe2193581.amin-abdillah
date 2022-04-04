fbea0c8eec3aaca99569338766802274e2e50d69
Amin Abdillah <andiabdillah255@gmail.com>
2022-04-04 21:46:26 +0700

    chore: Add answer for dom

--- frontend/dom/guess-number-assignment/index.js
+++ frontend/dom/guess-number-assignment/index.js
@@ -11,39 +11,50 @@ Uncomment variable dibawah ini untuk mulai mengerjakan. dilarang mengganti nama 
 // let score = nilai yang akan ditampilakan
 
 // TODO: answer here
-let randomNumber = document.getElementById('hidden-number');
-let inputNumber = document.getElementsByClassName('input');
-let message = document.getElementsByClassName('message');
-let score = document.getElementsByClassName('score');
+let randomNumber = Math.floor(Math.random() * 10) + 1;
+let input = document.querySelector('.input');
+let message = document.querySelector('.message');
+let score = document.querySelector('.score');
+let number = document.querySelector('.number');
 
 function displayMessage(msg) {
   // TODO: answer here
   message.innerHTML = msg;
 }
 
-document.getElementById('myBtn').addEventListener('click', function () {
-  myFunction(p1, p2);
-});
+
+score.innerHTML = 10;
+
+document.querySelector('.new-game').addEventListener('click', resetGame);
+document.querySelector('.check').addEventListener('click', checkNumber);
 
 function resetGame() {
   // TODO: answer here
   //dilarang menghapus code dibawah ini!
-  document.getElementById('hidden-number').innerHTML = randomNumber;
-  document.getElementsByClassName('input').innerHTML = '';
-  document.getElementsByClassName('message').innerHTML = '';
-  document.getElementsByClassName('score').innerHTML = '';
+  number.innerHTML = "?";
+  randomNumber = Math.floor(Math.random() * 10) + 1;
+  input.value = '';
+  message.innerHTML = 'Lets start guessing...';
+  score.innerHTML = 10;
 }
 
 function checkNumber() {
   // TODO: answer here
-  if(inputNumber.value < 0 || inputNumber.value > 10) {
-    displayMessage('Input number must be between 0 and 10');
-  }else if(inputNumber.value == randomNumber) {
-    displayMessage('You Win!');
-  } else if(inputNumber.value > randomNumber) {
-    displayMessage('Too High!');
-  } else if(inputNumber.value < randomNumber) {
-    displayMessage('Too Low!');
+  if(input.value ==='' || input.value == 0 || input.value < 0 || input.value > 10) {
+    displayMessage('Guess any number between 1 and 10');
+  } else if(input.value == randomNumber) {
+    displayMessage('Yeay! You guessed the correct number!');
+    number.innerHTML = randomNumber;
+  } else if(input.value > randomNumber) {
+    displayMessage("Oww... that's too big!");
+    score.innerHTML--;
+  } else if(input.value < randomNumber) {
+    displayMessage('Too small, buddy!');
+    score.innerHTML--;
+  }
+
+  if(score.innerHTML == 0) {
+    displayMessage('Game Over!');
   }
 }
 
