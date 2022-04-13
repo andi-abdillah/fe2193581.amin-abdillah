33e621fd9d92029c274535f60af83500c784b4cd
Amin Abdillah <andiabdillah255@gmail.com>
2022-04-13 10:03:18 +0700

    chore:add answer for oop-thief-and-trickster

--- frontend/javascript/11-oop-thief-and-trickster-assignment/index.js
+++ frontend/javascript/11-oop-thief-and-trickster-assignment/index.js
@@ -8,10 +8,33 @@ class Thief extends Player {
   
   robbedBlind() {
     // TODO: answer here
+    if(this.getGold()<10){
+      return "Aku terlalu miskin";
+    }else{
+      this.setGold(this.getGold()-10);
+      this.setStealChance(0.75);
+    }
   }
 
   steal(player) {
     // TODO: answer here
+    if(player.job === "Trickster"){
+      return player.distractionPurse(this);
+    }else{
+      if(player.getGold() < 5){
+        return "Lawan terlalu miskin";
+      }else{
+        if(this.randomizer() <= this.getStealChance()){
+          player.setGold(player.getGold() - 5);
+          this.setGold(this.getGold() + 5);
+          this.setHasBeenRobbed(true);
+          return "Berhasil mencuri 5 gold";
+        }else{
+          this.setHasBeenRobbed(false);
+          return "Gagal mencuri, coba lain kali";
+        }
+      }
+    }
   }
 }
 
@@ -24,21 +47,46 @@ class Trickster extends Player {
 
   setDistractionPurseChance(chance) {
     // TODO: answer here
+    this.distractionPurseChance = chance;
   }
 
   getDistractionPurseChance() {
     // TODO: answer here
+    return this.distractionPurseChance;
   }
 
   distractionPurse(player) {
     const rng = this.randomizer();
     // TODO: answer here
+    if(player.gold < 10 && rng < this.distractionPurseChance){
+      this.gold += player.gold;
+      player.gold = 0;
+      return "Berhasil mencuri balik semua uang lawan";
+    }else{
+      if(rng < this.distractionPurseChance){
+        this.gold += 5;
+        player.gold -= 5;
+        return "Berhasil mencuri balik 10 gold";
+      }else{
+        return "Gagal mencuri balik";
+      }
+    }
   }
 
   steal(player) {
     // TODO: answer here
   }
 }
+
+const p1 = new Thief("hehe");
+const p2 = new Trickster("kek");
+
+p1.setStealChance(1);
+    p2.setDistractionPurseChance(1);
+    p1.steal(p2);
+
+    const gold = p2.getGold();
+console.log(gold);
 
 // Dilarang menghapus code dibawah ini!!!
 module.exports = {
