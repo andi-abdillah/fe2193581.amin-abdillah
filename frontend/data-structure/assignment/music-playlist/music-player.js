a0b496b7d86e795c3bba6419ae753155fc334b64
Amin Abdillah <andiabdillah255@gmail.com>
2022-04-26 11:46:47 +0700

    chore: add answer for music-playlist assignment

--- frontend/data-structure/assignment/music-playlist/music-player.js
+++ frontend/data-structure/assignment/music-playlist/music-player.js
@@ -8,9 +8,35 @@ module.exports = class MusicPlayer {
 
     addSong(song) {
         // TODO: answer here
+        this.playlist.songs.push(song);
     }
 
     play() {
         // TODO: answer here
+        let output = "";
+        if(this.playlist.songs.length === 0) {
+            return output;
+        }
+        if(this.playlist.isRepeatable === true) {
+            output = `Now Playing ${this.playlist.songs[0].singer} - ${this.playlist.songs[0].title}`;
+            this.playlist.songs.push(this.playlist.songs.shift());
+        } else{
+            output = `Now Playing ${this.playlist.songs[0].singer} - ${this.playlist.songs[0].title}`;
+            this.playlist.songs.shift();
+        }
+        return output;
     }
 }
+
+
+const MusicPlayer = require('./music-player')
+
+let playlist = new Playlist([], false)
+let player = new MusicPlayer(playlist)
+let song = new Song("Tulus", "Hati-Hati di Jalan")
+player.addSong(song)
+console.log(player)
+console.log(song)
+console.log(typeof song)
+console.log(typeof player.playlist.songs)
+console.log(player.playlist.songs)
