e03b02e76687d3518fa44f10330ab7eea8d27b36
Amin Abdillah <andiabdillah255@gmail.com>
2022-04-27 09:45:00 +0700

    chore: add answer for text-editor assignment

--- frontend/data-structure/assignment/text-editor/index.js
+++ frontend/data-structure/assignment/text-editor/index.js
@@ -1,21 +1,47 @@
 module.exports = class TextEditor {
     constructor() {
         // TODO: answer here
+        this.data = [];
+        this.undoStack = [];
+        this.redoStack = [];
     }
 
     write(c) {
         // TODO: answer here
+        this.data.push(c);
+        this.undoStack.push(c);
     }
 
     read() {
         // TODO: answer here
+        return this.data.join('');
     }
 
     undo() {
         // TODO: answer here
+        if(this.data.length > 0){
+            this.undoStack.pop();
+            this.redoStack.push(this.data.pop());
+        }
     }
     
     redo() {
         // TODO: answer here
+        if(this.redoStack.length > 0){
+            this.undoStack.push(this.redoStack[-1]);
+            this.data.push(this.redoStack.pop());
+        }
     }
 }
+
+// let te = new TextEditor()
+// te.write("a")
+// console.log(te.undoStack)
+// te.undo()
+// console.log(te.redoStack)
+// te.redo()
+// console.log(te.data)
+// let output = te.read()
+// console.log(output)
+// console.log(te.undoStack.length)
+// console.log(te.redoStack.length)
