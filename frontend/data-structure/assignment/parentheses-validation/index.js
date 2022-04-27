6b70a7b89aacfc71e03ff3ac855c0b07c226acb1
Amin Abdillah <andiabdillah255@gmail.com>
2022-04-27 19:57:58 +0700

    chore: add answer for parentheses-validation assignment

--- frontend/data-structure/assignment/parentheses-validation/index.js
+++ frontend/data-structure/assignment/parentheses-validation/index.js
@@ -2,8 +2,39 @@ const Stack = require('./stack')
 
 function IsValidParentheses(s) {
     // TODO: answer here
+    let stack = new Stack()
+    let map = {
+        '{': '}',
+        '[': ']',
+        '(': ')'
+    }
+    if(s.length === 0){
+        return false;
+    }
+    if(s.length % 2 !== 0){
+        return false;
+    }
+    for(let i = 0; i < s.length; i++) {
+        if(s[i] in map) {
+            stack.push(s[i])
+            for(let j = i+1; j < s.length; j++){
+                if(s[j] === map[stack.peek()]){
+                    stack.pop();
+                    break;
+                }
+            }
+        }
+    }
+    console.log(stack.data)
+    if(stack.data.length === 0){
+        return true;
+    } else{
+        return false;
+    }
 }
 
+let got = IsValidParentheses("{[()]}")
+console.log(got)
 module.exports = {
     IsValidParentheses
 }
