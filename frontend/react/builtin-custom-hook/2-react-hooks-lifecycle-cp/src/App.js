661a3df638f20e963874c8b53586ee377bd078cb
Amin Abdillah <andiabdillah255@gmail.com>
2022-05-10 21:20:53 +0700

    chore: add answer for hooks-state-cp and hooks-lifecycle-cp

--- frontend/react/builtin-custom-hook/2-react-hooks-lifecycle-cp/src/App.js
+++ frontend/react/builtin-custom-hook/2-react-hooks-lifecycle-cp/src/App.js
@@ -6,8 +6,14 @@ export default function App() {
 
 export function RandomQuote() {
   // TODO: answer here
+  const [quote, setQuote] = React.useState('');
+  const [loading, setLoading] = React.useState(true);
   React.useEffect(() => {
     // TODO: answer here
+    setLoading(true)
+    fetch('https://api.quotable.io/random').then(res => res.json())
+    .then(data => setQuote(data.content))
+    .finally(() => setLoading(false))
   }, []);
 
   function getQuote() {
@@ -19,4 +25,17 @@ export function RandomQuote() {
   }
 
   // TODO: answer here
+  return (
+    <div className="wrapper">
+      <h1 className="title" data-testid="title">
+        Random Quote
+      </h1>
+      <h1 className="quote" data-testid="quote">
+        {loading ? "Loading..." : quote.content}
+      </h1>
+      <button onClick={getQuote} className="button" data-testid="button" >
+        Get another quote
+      </button>
+    </div>
+  );
 }
